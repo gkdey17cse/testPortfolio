8b37d1116c46a2bd97cb23187bf20a9f66ced862
gkdey17cse <gkdey17cse@gmail.com>
2025-07-06 06:39:03 +0530

    Update server.js & add vercel.json

--- server.js
+++ server.js
@@ -1,43 +1,46 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
 
 // Serve static files (CSS, JS, images)
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes for each page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 app.get('/experience', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'experience.html'))
-})
+    res.sendFile(path.join(__dirname, 'views', 'experience.html'));
+});
+
 app.get('/achievements', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'responsibilitiesAchievements.html'))
-})
+    res.sendFile(path.join(__dirname, 'views', 'responsibilitiesAchievements.html'));
+});
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'contact.html'))
-})
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
 app.get('/blogs', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'blogs.html'))
-})
+    res.sendFile(path.join(__dirname, 'views', 'blogs.html'));
+});
+
 app.get('/education', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'education&certification.html'))
-})
+    res.sendFile(path.join(__dirname, 'views', 'education&certification.html'));
+});
+
 app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'projects.html'))
-})
+    res.sendFile(path.join(__dirname, 'views', 'projects.html'));
+});
 
 app.get('/skills', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'skills.html'))
-})
-// Add more routes: /projects, /education, etc.
+    res.sendFile(path.join(__dirname, 'views', 'skills.html'));
+});
 
-const PORT = process.env.PORT || 3004
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
+// ✅ Export app for Vercel (instead of listen)
+module.exports = app;
